4c30615930ac651070c0e0abc10bb0fb5dc0a9fe
AntonNguyen-1 <anton.zxwxc@gmail.com>
2021-05-22 16:31:59 +0300

    create empty components

---
+++ src/components/CurrencyFilter.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CurrencyFilter() {
+  return <div></div>;
+}
---
+++ src/components/TicketItem.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TicketItem() {
+  return <div></div>;
+}
