86f1529495c74b1f3e66948c0eecde8cb05eebaf
AntonNguyen-1 <anton.zxwxc@gmail.com>
2021-05-26 01:08:29 +0300

    replace mocked data object to fetch local json

--- src/redux/ticket/ticket.api.ts
+++ src/redux/ticket/ticket.api.ts
@@ -1,13 +1,14 @@
 import { AnyAction } from "redux";
 import { ThunkAction } from "redux-thunk";
 import { RootState } from "../store";
-import mockResponse from "../../data/tickets.json";
 import { addItems } from "./ticket.action";
 import { Currency } from "../currency/types/types";
+import { Ticket } from "./types/types";
 
 export const fetchTickets =
   (): ThunkAction<void, RootState, unknown, AnyAction> => async (dispatch) => {
-    const data = mockResponse;
+    const response = await fetch("/src/data/tickets.json");
+    const data: { tickets: Ticket[] } = await response.json();
     const tickets = data.tickets.map((t) => {
       return { ...t, currentCurrency: Currency.RUB };
     });
